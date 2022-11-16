e34eb1b0001af287203589373efef7aa0ad51d71
Anderson Ves <andersonvesg@gmail.com>
2022-11-16 17:10:42 -0300

    Login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,21 +13,46 @@ import { Context } from "../context";
 
 const Login = () => {
 
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(false)
 
-    const handleSubmit = () => {
+    const [_user, setUser] = useContext(Context)
+    const navigate = useNavigate()
+    
+    const handleSubmit = async () => {
+        setError(false)
+        try {
+            const requestToken = await API.getRequestToken()
+            const sessionId = await API.authenticate(
+                requestToken,
+                username,
+                password
+            )   
+            console.log(sessionId);
+            setUser({sessionId: sessionId.session_id, username})
 
+            navigate('/')
+
+        } catch (error) {
+            setError(true)
+        }
     }
 
     const handleInput = e => {
+        const name = e.currentTarget.name
+        const value = e.currentTarget.value
 
+        if(name==='username') setUsername(value)
+        if(name==='password') setPassword(value)
     }
 
     return (
         <Wrapper>
             <label>Username:</label>
-            <input type='text' value='' name='username' onChange={handleInput} />
+            <input type='text' value={username} name='username' onChange={handleInput} />
             <label>Password:</label>
-            <input type='password' value='' name='password' onChange={handleInput} />
+            <input type='password' value={password} name='password' onChange={handleInput} />
             <Button text='Login' callback={handleSubmit} />
         </Wrapper>
     )
