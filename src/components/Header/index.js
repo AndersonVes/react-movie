0706ad1d19c9eead5cb887e83abd3be06a125ac0
Anderson Ves <andersonvesg@gmail.com>
2022-11-16 17:29:10 -0300

    Login on Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -7,15 +7,31 @@ import TMDBLogo from '../../images/tmdb_logo.svg';
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 
-const Header = () => (
-    <Wrapper>
-        <Content>
-            <Link to='/'>
-                <LogoImg src={RMDBLogo} alt='Rmdb logo' />
-            </Link>
+//Context
+import { Context } from '../../context';
+
+const Header = () => {
+    const [user] = useContext(Context)
+    console.log(user);
+
+    return (
+        <Wrapper>
+            <Content>
+                <Link to='/'>
+                    <LogoImg src={RMDBLogo} alt='Rmdb logo' />
+                </Link>
+                {user ? (
+                    <span>Loggedin as {user.username}</span>)
+                    : (
+                        <Link to='/login'>
+                            <span>Login</span>
+                        </Link>
+                    )
+                }
                 <TMDBLogoImg src={TMDBLogo} alt='Tmdb logo' />
-        </Content>
-    </Wrapper>
-);
+            </Content>
+        </Wrapper>
+    );
+}
 
 export default Header;
