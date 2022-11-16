75f0e712c7eee77539d5ba9236585da863c7aaa9
Anderson Ves <andersonvesg@gmail.com>
2022-11-16 16:44:36 -0300

    Context, UserProvider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 //Routing
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
+//Context
+import UserProvider from './context';
+
+
 //Components
 import Header from './components/Header'
 import Home from './components/Home';
@@ -15,14 +19,16 @@ import { GlobalStyle } from './GlobalStyle';
 const App = () => {
   return (
     <Router>
-      <Header/>
+    <UserProvider>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/:movieId' element={<Movie/>}/>
-        <Route path='/*' element={<NotFound/>}/>
-      </Routes>        
-      <GlobalStyle/>
-    </Router>
+        <Route path='/' element={<Home />} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
+    </UserProvider>
+  </Router>
   );
 }
 
