9a5854e8bff2532ac24fb346527f59eb1ce08b16
Anderson Ves <andersonvesg@gmail.com>
2022-11-16 17:17:56 -0300

    Login style

--- src/components/Login.styles.js
+++ src/components/Login.styles.js
@@ -1,5 +1,26 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    display: flex,
+    align-items: center,
+    justify-content: center;
+    flex-direction: column;
+    margin: 0 auto;
+    max-width: 320px;
+    padding: 20px;
+    color: var(--darkGrey);
+    text-align: center;
 
-export const Content = styled.div``;
+    input {
+        width: 100%;
+        height: 30px;
+        border: 1px solid var(--darkGrey);
+        border-radius: 20px;
+        margin: 10px 0;
+        padding: 10px;
+    }
+
+    .error {
+        color: red
+    }
+`;
